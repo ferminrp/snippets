35838910841f84dd638f8214649ec0452c269ac8
ferminrp <frodriguezpenelas@gmail.com>
2020-06-10 19:12:44 -0300

    Please enter the commit message for your changes. Lines starting

---
+++ Python & Data Science/Numpy/generar-random-array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Definimos el tamaño del array que vamos a construir
+big_array_size = 1000000
+
+# Definimos la semilla del generador random
+seed_cualquier_numero = 2843
+
+# Creamos el generador
+random_generator_seed = np.random.default_rng(seed_cualquier_numero)
+
+# Creamos el array con elementos de distribución uniforme
+low = 1
+high = 100
+big_array = random_generator_seed.uniform(low, high, size=big_array_size)
